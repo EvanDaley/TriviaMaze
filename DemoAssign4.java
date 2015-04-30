27f320ea780f273047329bf0a8f9ae81d21951ab
pgort <pgort@users.noreply.github.com>
2015-04-30 09:35:15 -0700

    Cha-cha-cha-changes!
    Nothing important changed. Srsly.

--- DemoAssign4.java
+++ DemoAssign4.java
@@ -1,8 +1,9 @@
+//NEW BRANCH
+
 public class DemoAssign4
 {
-public static void main (String [] args)
-{
-	System.out.println("Yeah Modified the Branch")
-}	
-	
+   public static void main (String [] args)
+   {
+   	System.out.println("Lazy, good-for-nothing program.")
+   }
 }
