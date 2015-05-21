4de68ba2d8000f88a66ae74ad6bced743149a7e9
Michael Parkes <michaelparkes.0608@gmail.com>
2015-05-21 13:58:41 -0700

    Committing Tools for DB Connection

---
+++ DatabaseTools.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Data;
+using Mono.Data.SqliteClient;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Data.Sql;
+
+public static class DatabaseTools
+{
+	// move database functions into this static helper class
+
+
+}
